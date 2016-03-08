a94f0cd6e77a9a7240c8a711dff706eb24af624d
gzeyu <gzeyu@192.168.60.68>
2016-03-08 18:16:08 +0800

    有趣代码

---
+++ src/main/java/com/dimit/overload/InterestOverload.java
@@ -0,0 +1,28 @@
+package com.dimit.overload;
+
+/**
+ * 有趣的重载
+ * 
+ * @author dimit
+ */
+public class InterestOverload {
+	/*public void hardChose(List<Integer> il) {
+		System.out.println("list integer...");
+	}
+	//在JDK1.6之前这段代码可以通过改变返回值来通过编译
+	public void hardChose(List<String> sl) {
+		System.out.println("list string ...");
+	}*/
+	
+	
+	
+	/*public static int hardChose(List<Integer> list) {
+		System.out.println("list integer...");
+		return 0;
+	}
+	//在JDK1.6之前这段代码可以通过编译
+	public static String hardChose(List<String> list) {
+		System.out.println("list string ...");
+		return "";
+	}*/
+}
